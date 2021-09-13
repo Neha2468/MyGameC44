09788b4d66009e6160539ffc92198585090d8363
Neha2468 <81560725+Neha2468@users.noreply.github.com>
2021-09-13 20:17:41 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var runners, runner1, runner2, runner3, runner4;
+var runner1Img, runner2Img, runner3Img, runner4Img;
+var canvas;
+var database;
+
+
+
+
+function preload (){
+
+}
+
+
+
+
+
+
+function setup(){
+   canvas = createCanvas(windowWidth, windowHeight);
+   var database = firebase.database();
+   
+
+
+}
+
+
+
+
+
+
+function draw(){
+
+}
